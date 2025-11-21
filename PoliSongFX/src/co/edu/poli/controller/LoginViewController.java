980d6fc537a470603eecadf6e86a60c60160efc3
Sebastian Novoa <sebasanabria868@gmail.com>
2025-11-21 13:22:55 -0500

    Merge branch 'main' of https://github.com/szaratep/PC_PoliSopngFX.git into main

--- PoliSongFX/src/co/edu/poli/controller/LoginViewController.java
+++ PoliSongFX/src/co/edu/poli/controller/LoginViewController.java
@@ -1,5 +1,34 @@
 package co.edu.poli.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import co.edu.poli.negocio.*;
 public class LoginViewController {
+
+    @FXML
+    private Label mensajeSesion;
+
+    @FXML
+    private TextField passwordSesion;
+
+    @FXML
+    private TextField userSesion;
+    
+    authManager auth = new authManager();
+    @FXML
+    void RegistroSesion(ActionEvent event) {
+    	
+    }
+
+    Boolean flag = auth.loginUsuario(userSesion.getText(), passwordSesion.getText());
+    
+    @FXML
+    void inicioSesion(ActionEvent event) {
+    	if (auth.loginUsuario(userSesion.getText(), passwordSesion.getText()) == true){
+    		mensajeSesion.setText("hola");
+    	}
+    }
 
 }
