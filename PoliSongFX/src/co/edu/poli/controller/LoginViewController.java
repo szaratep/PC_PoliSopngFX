a8308a62f8f48727a7cc25673ce3f56a104304ee
Sebastian Novoa <sebasanabria868@gmail.com>
2025-11-21 15:04:48 -0500

    RegisterView terminado y login completo

--- PoliSongFX/src/co/edu/poli/controller/LoginViewController.java
+++ PoliSongFX/src/co/edu/poli/controller/LoginViewController.java
@@ -2,8 +2,12 @@ package co.edu.poli.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import co.edu.poli.negocio.*;
 public class LoginViewController {
 
@@ -19,16 +23,43 @@ public class LoginViewController {
     authManager auth = new authManager();
     @FXML
     void RegistroSesion(ActionEvent event) {
+    	try {
+        	FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/RegisterView.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error al abrir RegisterView.fxml: " + e.getMessage());
+        }
     	
     }
-
-    Boolean flag = auth.loginUsuario(userSesion.getText(), passwordSesion.getText());
     
-    @FXML
+    @FXML 
     void inicioSesion(ActionEvent event) {
     	if (auth.loginUsuario(userSesion.getText(), passwordSesion.getText()) == true){
-    		mensajeSesion.setText("hola");
+    		mensajeSesion.setText("Se inicio sesion correctamente como Usuario");
     	}
+    	else {
+    		mensajeSesion.setText("Usuario o contraseña incorrecta");
+    	}
+    }
+    
+    @FXML
+    void volverInicio(ActionEvent event) {
+    	try {
+        	FXMLLoader loader = new FXMLLoader(getClass().getResource("/application/mainPage.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setTitle("Login");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error al abrir mainPage.fxml: " + e.getMessage());
+        }
     }
 
 }
